d711d972da026405e6d7d18fe30ee7e09b681c54
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-03-20 15:42:44 +0800

    completed redrafting 1

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyscroll()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -8,10 +8,17 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyscroll() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+var typed = new Typed(".auto-type", {
+  strings: ["Welcome to aikiForest", "The educational powerhouse in contemporary aikido", "We deliver and develop high quality and learner-centric aikido programmes"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+})
