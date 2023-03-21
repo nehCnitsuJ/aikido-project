8c4ff4c450cb9fc32d0cbf045ff51279fcc98ccd
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-03-21 11:48:51 +0800

    class section for homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickyscroll()};
+window.onscroll = function () {
+  stickyscroll();
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -10,7 +12,7 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyscroll() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
@@ -18,29 +20,33 @@ function stickyscroll() {
 
 //Type writer animation
 var typed = new Typed(".auto-type", {
-  strings: ["Welcome to aikiForest", "The educational powerhouse in contemporary aikido", "We deliver and develop high quality and learner-centric aikido programmes"],
+  strings: [
+    "Welcome to aikiForest",
+    "The educational powerhouse in contemporary aikido",
+    "We deliver and develop high quality and learner-centric aikido programmes",
+  ],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
-})
+});
 
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btnimg');
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btnimg");
 let currentSlide = 1;
 
 // Javascript for image slider manual navigation
-var manualNav = function(manual){
+var manualNav = function (manual) {
   slides.forEach((slide) => {
-    slide.classList.remove('active');
+    slide.classList.remove("active");
 
     btns.forEach((btn) => {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     });
   });
 
-  slides[manual].classList.add('active');
-  btns[manual].classList.add('active');
-}
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
 
 btns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
@@ -50,29 +56,29 @@ btns.forEach((btn, i) => {
 });
 
 // Javascript for image slider autoplay navigation
-var repeat = function(activeClass){
-  let active = document.getElementsByClassName('active');
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
   let i = 1;
 
   var repeater = () => {
-    setTimeout(function(){
+    setTimeout(function () {
       [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove('active');
+        activeSlide.classList.remove("active");
       });
 
-    slides[i].classList.add('active');
-    btns[i].classList.add('active');
-    i++;
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
 
-    if(slides.length == i){
-      i = 0;
-    }
-    if(i >= slides.length){
-      return;
-    }
-    repeater();
-  }, 10000);
-  }
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 10000);
+  };
   repeater();
-}
+};
 repeat();
