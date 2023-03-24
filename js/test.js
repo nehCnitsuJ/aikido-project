0f963031d851069bdd5148c92508bd653fc9aa7a
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-03-24 14:45:26 +0800

    layout formatting

--- js/test.js
+++ js/test.js
@@ -44,3 +44,22 @@ function stickyscroll() {
     navbar.classList.remove("sticky");
   }
 }
+
+//Scroll upwards
+// window.onload = function () {
+//   const effect = document.querySelector("#effect");
+
+//   window.addEventListener("scroll", scrollEffect);
+
+//   function scrollEffect() {
+//     if (window.scrollY >= 500) {
+//       effect.style.opacity = "1";
+//       effect.style.transform = "translateX(0px)";
+//       effect.style.transition = "1s ease-in-out";
+//     } else {
+//       effect.style.opacity = "0";
+//       effect.style.transform = "translateX(-50px)";
+//     }
+//   }
+//   scrollEffect;
+// };
