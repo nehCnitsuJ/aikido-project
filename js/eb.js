3fcaf02f3f625c4affbfb6683cf4913da0366f2f
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-04-10 16:37:37 +0800

    created js file for display

---
+++ js/eb.js
@@ -0,0 +1,8 @@
+function openBelt() {
+  var belt = document.getElementById("#video-section");
+  if (belt.style.display === "none") {
+    belt.style.display = "block";
+  } else {
+    belt.style.display = "none";
+  }
+}
