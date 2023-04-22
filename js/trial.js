52e300abcf331d06de2fc2f124f4c14215d31a9a
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-04-22 16:15:35 +0800

    trial form completed

--- js/trial.js
+++ js/trial.js
@@ -3,9 +3,17 @@ const personTrial = document.querySelector("#personTrial");
 const meAttr = document.getElementsByClassName("me-attr");
 const childAttr = document.getElementsByClassName("child-attr");
 const generalAttr = document.getElementsByClassName("general-attr");
+const locationTrial = document.getElementById("location");
+
 const arcAttr = document.querySelector(".arc-attr");
+const arcSch = document.getElementById("arc-schedule");
+
 const stpyAttr = document.querySelector(".stpy-attr");
+const stpySch = document.getElementById("stpy-schedule");
+
 const rvAttr = document.querySelector(".rv-attr");
+const rvSch = document.getElementById("rv-schedule");
+
 const submitBtn = document.querySelector(".submitBtn");
 
 //On select, remove/add hidden class based on what is displayed
@@ -57,37 +65,80 @@ personTrial.addEventListener("change", (event) => {
 });
 
 //On select, change to different location outlet
-Array.from(generalAttr).forEach((generalAttr) => {
-  generalAttr.addEventListener("change", (event) => {
-    if (
-      event.target.id === "arc1" ||
-      event.target.id === "arc2" ||
-      event.target.id === "arc3"
-    ) {
-      arcAttr.classList.remove("hidden");
-      stpyAttr.classList.add("hidden");
-      rvAttr.classList.add("hidden");
-      submitBtn.classList.remove("hidden");
-    } else if (
-      event.target.id === "stpy1" ||
-      event.target.id === "stpy2" ||
-      event.target.id === "stpy3"
-    ) {
-      arcAttr.classList.add("hidden");
-      stpyAttr.classList.remove("hidden");
-      rvAttr.classList.add("hidden");
-      submitBtn.classList.remove("hidden");
-    } else if (
-      event.target.id === "rv1" ||
-      event.target.id === "rv2" ||
-      event.target.id === "rv3"
-    ) {
-      arcAttr.classList.add("hidden");
-      stpyAttr.classList.add("hidden");
-      rvAttr.classList.remove("hidden");
-      submitBtn.classList.remove("hidden");
-    }
-  });
+locationTrial.addEventListener("change", (event) => {
+  if (event.target.value === "arc") {
+    arcAttr.classList.remove("hidden");
+    arcSch.removeAttribute("disabled");
+
+    stpyAttr.classList.add("hidden");
+    stpySch.setAttribute("disabled", true);
+
+    rvAttr.classList.add("hidden");
+    rvSch.setAttribute("disabled", true);
+
+    submitBtn.classList.remove("hidden");
+  } else if (event.target.value === "stpy") {
+    arcAttr.classList.add("hidden");
+    arcSch.setAttribute("disabled", true);
+
+    stpyAttr.classList.remove("hidden");
+    stpySch.removeAttribute("disabled");
+
+    rvAttr.classList.add("hidden");
+    rvSch.setAttribute("disabled", true);
+
+    submitBtn.classList.remove("hidden");
+  } else if (event.target.value === "rv") {
+    arcAttr.classList.add("hidden");
+    arcSch.setAttribute("disabled", true);
+
+    stpyAttr.classList.add("hidden");
+    stpySch.setAttribute("disabled", true);
+
+    rvAttr.classList.remove("hidden");
+    arcSch.removeAttribute("disabled");
+
+    submitBtn.classList.remove("hidden");
+  }
+});
+
+arcSch.addEventListener("change", (event) => {
+  if (
+    event.target.id === "arc1" ||
+    event.target.id === "arc2" ||
+    event.target.id === "arc3"
+  ) {
+    arcAttr.classList.remove("hidden");
+    stpyAttr.classList.add("hidden");
+    rvAttr.classList.add("hidden");
+    submitBtn.classList.remove("hidden");
+  }
+});
+
+stpySch.addEventListener("change", (event) => {
+  if (
+    event.target.id === "stpy1" ||
+    event.target.id === "stpy2" ||
+    event.target.id === "stpy3"
+  ) {
+    arcAttr.classList.add("hidden");
+    stpyAttr.classList.remove("hidden");
+    rvAttr.classList.add("hidden");
+    submitBtn.classList.remove("hidden");
+  }
+});
+
+rvSch.addEventListener("change", (event) => {
+  if (
+    event.target.id === "rv1" ||
+    event.target.id === "rv2" ||
+    event.target.id === "rv3"
+  ) {
+    arcAttr.classList.add("hidden");
+    stpyAttr.classList.add("hidden");
+    rvAttr.classList.remove("hidden");
+    submitBtn.classList.remove("hidden");
+  }
 });
 
 //On change, show submit button
