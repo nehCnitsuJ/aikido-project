be087bd8f4885aa1ec69406bf605923c52bdb62d
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-04-24 12:44:06 +0800

    changed splash screen to local storage

--- js/home.js
+++ js/home.js
@@ -2,25 +2,60 @@ let splashscreen = document.querySelector(".splash-screen");
 let ssheading = document.querySelector(".ss-heading");
 let msg = document.querySelectorAll(".msg");
 
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    msg.forEach((span, idx) => {
-      setTimeout(() => {
-        span.classList.add("active");
-      }, (idx + 1) * 400);
-    });
-
+if (!localStorage.seenScreen) {
+  window.addEventListener("DOMContentLoaded", () => {
+    localStorage.seenScreen = true;
     setTimeout(() => {
       msg.forEach((span, idx) => {
         setTimeout(() => {
-          span.classList.remove("active");
-          span.classList.add("fade");
-        }, (idx + 1) * 50);
+          span.classList.add("active");
+        }, (idx + 1) * 400);
       });
-    }, 2000);
 
-    setTimeout(() => {
-      splashscreen.style.top = "-100vh";
-    }, 2300);
+      setTimeout(() => {
+        msg.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.remove("active");
+            span.classList.add("fade");
+          }, (idx + 1) * 50);
+        });
+      }, 2000);
+
+      setTimeout(() => {
+        splashscreen.style.top = "-100vh";
+      }, 2300);
+    });
   });
-});
+} else {
+  splashscreen.classList.add("hidden");
+}
+
+// $(document).ready(function () {
+//   if (sessionStorage.getItem("splash-screen") !== "true") {
+//     window.addEventListener("DOMContentLoaded", () => {
+//       setTimeout(() => {
+//         msg.forEach((span, idx) => {
+//           setTimeout(() => {
+//             span.classList.add("active");
+//           }, (idx + 1) * 400);
+//         });
+
+//         setTimeout(() => {
+//           msg.forEach((span, idx) => {
+//             setTimeout(() => {
+//               span.classList.remove("active");
+//               span.classList.add("fade");
+//             }, (idx + 1) * 50);
+//           });
+//         }, 2000);
+
+//         setTimeout(() => {
+//           splashscreen.style.top = "-100vh";
+//         }, 2300);
+//       });
+//     });
+//     sessionStorage.setItem("splash-screen", "true");
+//   } else {
+//     $(".home-page").fadeIn();
+//   }
+// });
