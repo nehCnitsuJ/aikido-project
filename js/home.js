c0cb2d2a0963dd4fae8eb28ee130d82a937d30b4
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-04-25 15:13:06 +0800

    changed localStorage to sessionStorage

--- js/home.js
+++ js/home.js
@@ -2,9 +2,41 @@ let splashscreen = document.querySelector(".splash-screen");
 let ssheading = document.querySelector(".ss-heading");
 let msg = document.querySelectorAll(".msg");
 
-if (!localStorage.seenScreen) {
-  window.addEventListener("DOMContentLoaded", () => {
-    localStorage.seenScreen = true;
+// if (!localStorage.seenScreen) {
+//   window.addEventListener("DOMContentLoaded", () => {
+//     localStorage.seenScreen = true;
+//     setTimeout(() => {
+//       msg.forEach((span, idx) => {
+//         setTimeout(() => {
+//           span.classList.add("active");
+//         }, (idx + 1) * 400);
+//       });
+
+//       setTimeout(() => {
+//         msg.forEach((span, idx) => {
+//           setTimeout(() => {
+//             span.classList.remove("active");
+//             span.classList.add("fade");
+//           }, (idx + 1) * 50);
+//         });
+//       }, 2000);
+
+//       setTimeout(() => {
+//         splashscreen.style.top = "-100vh";
+//       }, 2300);
+//     });
+//   });
+// } else {
+//   splashscreen.classList.add("hidden");
+// }
+
+$(document).ready(function () {
+  if (sessionStorage.getItem("splash-screen") !== "true") {
+    console.log("here executed");
+    $(".splash-screen").show();
+    console.log(splashscreen);
+    console.log("splash screen shown");
+    console.log("here executed again");
     setTimeout(() => {
       msg.forEach((span, idx) => {
         setTimeout(() => {
@@ -25,37 +57,8 @@ if (!localStorage.seenScreen) {
         splashscreen.style.top = "-100vh";
       }, 2300);
     });
-  });
-} else {
-  splashscreen.classList.add("hidden");
-}
-
-// $(document).ready(function () {
-//   if (sessionStorage.getItem("splash-screen") !== "true") {
-//     window.addEventListener("DOMContentLoaded", () => {
-//       setTimeout(() => {
-//         msg.forEach((span, idx) => {
-//           setTimeout(() => {
-//             span.classList.add("active");
-//           }, (idx + 1) * 400);
-//         });
-
-//         setTimeout(() => {
-//           msg.forEach((span, idx) => {
-//             setTimeout(() => {
-//               span.classList.remove("active");
-//               span.classList.add("fade");
-//             }, (idx + 1) * 50);
-//           });
-//         }, 2000);
-
-//         setTimeout(() => {
-//           splashscreen.style.top = "-100vh";
-//         }, 2300);
-//       });
-//     });
-//     sessionStorage.setItem("splash-screen", "true");
-//   } else {
-//     $(".home-page").fadeIn();
-//   }
-// });
+    sessionStorage.setItem("splash-screen", "true");
+  } else {
+    $(".splash-screen").hide();
+  }
+});
